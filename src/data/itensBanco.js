2e406affdacacc9d430c42d074592aff3a297211
jjamersonmt <jjamersonmt@gmail.com>
2025-01-20 12:33:14 -0300

    inserido imagem dos outro itens

--- src/data/itensBanco.js
+++ src/data/itensBanco.js
@@ -1,13 +1,13 @@
 const itensBanco = [
   { id: 10, nome: 'cadeira', imagem: 'itens/cadeira.png', tipo: 'plastico', coletado: true },
   { id: 11, nome: 'garrafa de plastico', imagem: 'itens/garrafaplastica.png', tipo: 'plastico', coletado: true },
-  { id: 12, nome: 'Escudo', imagem: 'itens/pilha.png', tipo: 'metal', coletado: false },
-  { id: 13, nome: 'Poção de Vida', imagem: 'itens/pilha.png', tipo: 'vidro', coletado: false },
-  { id: 14, nome: 'Arco', imagem: 'itens/pilha.png', tipo: 'madeira', coletado: false },
-  { id: 15, nome: 'Flecha', imagem: 'itens/pilha.png', tipo: 'madeira', coletado: false },
-  { id: 16, nome: 'Capacete', imagem: 'itens/pilha.png', tipo: 'metal', coletado: false },
-  { id: 17, nome: 'Armadura', imagem: 'itens/pilha.png', tipo: 'metal', coletado: false },
-  { id: 18, nome: 'Botas', imagem: 'itens/pilha.png', tipo: 'couro', coletado: false },
+  { id: 12, nome: 'pilha', imagem: 'itens/pilha.png', tipo: 'eletronico', coletado: false },
+  { id: 13, nome: 'metais', imagem: 'itens/metal.png', tipo: 'metal', coletado: false },
+  { id: 14, nome: 'livro', imagem: 'itens/livro.png', tipo: 'papel', coletado: false },
+  { id: 15, nome: 'maçã', imagem: 'itens/macã.png', tipo: 'organico', coletado: false },
+  { id: 16, nome: 'casca de banana', imagem: 'itens/cascadebanana.png', tipo: 'organico', coletado: false },
+  { id: 17, nome: 'sacola plastica', imagem: 'itens/sacola plastica.png', tipo: 'plastico', coletado: false },
+  { id: 18, nome: 'dinheiro', imagem: 'itens/dinheiro.png', tipo: 'papel', coletado: false },
   { id: 19, nome: 'Anel', imagem: 'itens/pilha.png', tipo: 'metal', coletado: false },
   { id: 20, nome: 'Colar', imagem: 'itens/pilha.png', tipo: 'metal', coletado: false }
 ];
