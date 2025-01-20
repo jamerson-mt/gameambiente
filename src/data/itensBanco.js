3442038b583729ca78c38cb7d35c70b2f16a9e3b
jjamersonmt <jjamersonmt@gmail.com>
2025-01-20 12:13:23 -0300

    ajuste url de itens

--- src/data/itensBanco.js
+++ src/data/itensBanco.js
@@ -1,15 +1,15 @@
 const itensBanco = [
-  { id: 10, nome: 'cadeira', imagem: 'public/itens/cadeira.png', tipo: 'plastico', coletado: true },
-  { id: 11, nome: 'garrafa de plastico', imagem: 'public/itens/garrafaplastica.png', tipo: 'plastico', coletado: true },
-  { id: 12, nome: 'Escudo', imagem: 'public/itens/pilha.png', tipo: 'metal', coletado: false },
-  { id: 13, nome: 'Poção de Vida', imagem: 'public/itens/pilha.png', tipo: 'vidro', coletado: false },
-  { id: 14, nome: 'Arco', imagem: 'public/itens/pilha.png', tipo: 'madeira', coletado: false },
-  { id: 15, nome: 'Flecha', imagem: 'public/itens/pilha.png', tipo: 'madeira', coletado: false },
-  { id: 16, nome: 'Capacete', imagem: 'public/itens/pilha.png', tipo: 'metal', coletado: false },
-  { id: 17, nome: 'Armadura', imagem: 'public/itens/pilha.png', tipo: 'metal', coletado: false },
-  { id: 18, nome: 'Botas', imagem: 'public/itens/pilha.png', tipo: 'couro', coletado: false },
-  { id: 19, nome: 'Anel', imagem: 'public/itens/pilha.png', tipo: 'metal', coletado: false },
-  { id: 20, nome: 'Colar', imagem: 'public/itens/pilha.png', tipo: 'metal', coletado: false }
+  { id: 10, nome: 'cadeira', imagem: 'itens/cadeira.png', tipo: 'plastico', coletado: true },
+  { id: 11, nome: 'garrafa de plastico', imagem: 'itens/garrafaplastica.png', tipo: 'plastico', coletado: true },
+  { id: 12, nome: 'Escudo', imagem: 'itens/pilha.png', tipo: 'metal', coletado: false },
+  { id: 13, nome: 'Poção de Vida', imagem: 'itens/pilha.png', tipo: 'vidro', coletado: false },
+  { id: 14, nome: 'Arco', imagem: 'itens/pilha.png', tipo: 'madeira', coletado: false },
+  { id: 15, nome: 'Flecha', imagem: 'itens/pilha.png', tipo: 'madeira', coletado: false },
+  { id: 16, nome: 'Capacete', imagem: 'itens/pilha.png', tipo: 'metal', coletado: false },
+  { id: 17, nome: 'Armadura', imagem: 'itens/pilha.png', tipo: 'metal', coletado: false },
+  { id: 18, nome: 'Botas', imagem: 'itens/pilha.png', tipo: 'couro', coletado: false },
+  { id: 19, nome: 'Anel', imagem: 'itens/pilha.png', tipo: 'metal', coletado: false },
+  { id: 20, nome: 'Colar', imagem: 'itens/pilha.png', tipo: 'metal', coletado: false }
 ];
 
 export default itensBanco;
